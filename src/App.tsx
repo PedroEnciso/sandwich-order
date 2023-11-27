b0365daa1c9b84a30ea17707f894ee2aba1aac1a
Pedro Enciso <penciso@ucsd.edu>
2023-11-27 14:09:55 -0800

    display SandwichList

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import SandwichList from "src/components/SandwichContainer";
+
+import classes from "src/App.module.css";
+
 function App() {
-  return <>sandwich order app</>;
+  return (
+    <main>
+      <h1 className={classes.title}>Welcome to Pedro's sandwich shop</h1>
+      <p className={classes.description}>
+        Thank you for visiting our shop. Choose your sandwich, then customize
+        your toppings and place your order!
+      </p>
+      <SandwichList />
+    </main>
+  );
 }
 
 export default App;
